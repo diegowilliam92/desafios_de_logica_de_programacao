f84e24cef9a3b5920029a88f145424f5da31cba0
diego william <diegow262@gmail.com>
2023-05-27 18:57:14 -0300

    implementacao desafio cordenadas

--- treino_while02.java
+++ treino_while02.java
@@ -1,10 +1,34 @@
 package javaCompleto23;
 
+import java.util.Scanner;
+
 public class treino_while02 {
 
 	public static void main(String[] args) {
-		
-
+		Scanner sc = new Scanner( System.in);
+		int x,y;
+		System.out.println("informe o valor de x: ");
+		x=sc.nextInt();
+		System.out.println("informe o valor de y: ");
+		y=sc.nextInt();
+		while (x !=0 && y !=0) {
+			if(x>0 && y>0) {
+				System.out.println("primeiro");
+			}else if (x>0 && y<0) {
+				System.out.println("quarto");
+			}else if (x<0 && y<0) {
+				System.out.println("terceiro");
+			}else if (x<0 && y>0) {
+				System.out.println("segundo");
+			}
+			System.out.println("informe o valor de x novamente: ");
+			x=sc.nextInt();
+			System.out.println("informe o valor de y novamente: ");
+			y=sc.nextInt();	
+			}
+		sc.close();
+		}
+	
 	}
 
-}
+
